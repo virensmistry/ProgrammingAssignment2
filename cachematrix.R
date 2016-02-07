b7844ea8b396ec647e3cabb2a15f85b8ddc834f2
virensmistry <vmistr4@uic.edu>
2016-02-07 16:42:05 -0600

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Coded by Viren Mistry for online course on Coursera
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+##Initializing the function and predefining the size of the matrix to a square matrix for easy operation.
+## Using the code provided to find the mean I developed this code to find the inverse matrix
+makeCacheMatrix<- function(x=matrix(, n.rows=3, n.col=3))#VM
+{
+      mat<- NULL #initializing matrix to null.
+      setmatrix <- function(y) 
+      {
+            x <<- y
+            mat <<- NULL #
+      }
+      getmatrix <- function() x
+      setinverse <- function(solve) mat <<- solve
+      getinverse <- function() mat
+      list(setmatrix = setmatrix, getmatrix = getmatrix, 
+           setinverse = setinverse, getinverse = getinverse)
 }
 
+#Inversing the matrix if the same matrix is found in the cache using the below function.
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) 
+{
+  mat <- x$getinverse()
+  if(!is.null(mat)) 
+  {
+    message("getting cached data")
+    return(mat)
+  }
+  data <- x$getmatrix()
+  mat <- solve(data, ...)
+  x$setinverse(mat)
+  mat
 }
+## Coded by Viren Mistry for online course on Coursera
